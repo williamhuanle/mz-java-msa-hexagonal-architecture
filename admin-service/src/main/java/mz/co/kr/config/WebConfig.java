ef2cca44811a0ec75342e7fd63e4a5ee5d528af0
mz01-william-huanlt <williamhuanle@gmail.com>
2022-02-25 09:35:55 +0700

    switch adapter

--- admin-service/src/main/java/mz/co/kr/config/WebConfig.java
+++ admin-service/src/main/java/mz/co/kr/config/WebConfig.java
@@ -1,7 +1,6 @@
 package mz.co.kr.config;
 
 import mz.co.kr.adapters.EmployeeJpaAdapterImpl;
-//import mz.co.kr.adapters.EmployeeMongoDbAdapterImpl;
 import mz.co.kr.admin.domain.services.EmployeeServiceImpl;
 import mz.co.kr.admin.ports.api.EmployeeServicePort;
 import mz.co.kr.admin.ports.spi.EmployeePersistencePort;
